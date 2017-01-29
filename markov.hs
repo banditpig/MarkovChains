f6e14992dd381d527befc7df55fd58b168730806
mike-k-houghton <mike_k_houghton@yahoo.co.uk>
2017-01-29 14:47:16 +0000

    wip

--- markov.hs
+++ markov.hs
@@ -1,23 +1,28 @@
-
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
 
-import Data.List
 import qualified Data.Map as M
+import Data.List (sortOn)
+import Data.Ord  (Down (..))
 
-data  MChain a = Map a [(String, Int)] deriving (Show)
+data MChain a = Map a [(a, Int)] deriving (Show)
 type Words = [String]
 
-cw :: Words -> [(String, [(String, Int)] )]
-cw [] = []
-cw (x:y:[]) = [(x, [(y, 1)])]
-cw (w:w':ws) = (w, [(w', 1)]):cw (w':ws)
-cw _         = []
+links :: Words -> [(String, [(String, Int)] )]
+links []        = []
+links (x:y:[])  = [(x, [(y, 1)])]
+links (w:w':ws) = (w, [(w', 1)]):links (w':ws)
+links _         = []
+
+merge :: [(String, Int)] -> [(String, Int)] -> [(String, Int)]
+merge ps ps' = condense $ ps ++ ps'
+
+-- -- -- i.e. [("x", 1), ("y", 1) ("x",3)] -> [("x", 4), ("y", 1)] 
+condense :: [(String, Int)] -> [(String, Int)]
+condense = sortOn Down . M.toList . M.fromListWith (+) 
 
 chainWords :: Words -> M.Map String [(String, Int)]
-chainWords wds = M.fromListWith (f . (++)) (cw wds) where f = id
--- f will combine duplicate tuples ie [("x", 1), ("x",3)] -> [("x", 4)]
+chainWords = M.fromListWith merge . links 
 
 main :: IO ()
-main = do
-    print ""
+main = print ""
