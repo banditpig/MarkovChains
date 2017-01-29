11c08500f49a933b2c2a2c9b971a97d71ccb8964
mike-k-houghton <mike_k_houghton@yahoo.co.uk>
2017-01-29 17:46:50 +0000

    sorting and filtering

--- markov.hs
+++ markov.hs
@@ -2,9 +2,10 @@
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
 
 import qualified Data.Map as M
-import Data.List (sortOn)
-import Data.Ord  (Down (..))
-
+import Data.List ( sortBy)
+import Data.Char
+import Data.Ord  (Down (..), comparing)
+-- ls = [("a", 1),("b",2 ),("c",3 ),("d",4 ),("e",5 ),("f", 6)]
 data MChain a = Map a [(a, Int)] deriving (Show)
 type Words = [String]
 
@@ -19,10 +20,16 @@ merge ps ps' = condense $ ps ++ ps'
 
 -- -- -- i.e. [("x", 1), ("y", 1) ("x",3)] -> [("x", 4), ("y", 1)] 
 condense :: [(String, Int)] -> [(String, Int)]
-condense = sortOn Down . M.toList . M.fromListWith (+) 
+condense = sortBy (comparing (Down . snd))  . M.toList . M.fromListWith (+) 
+
 
 chainWords :: Words -> M.Map String [(String, Int)]
 chainWords = M.fromListWith merge . links 
 
 main :: IO ()
-main = print ""
+main = do
+  input <- readFile "speeches.txt"
+
+  print $ chainWords . map (filter (\x -> ord x > 32 && ord x < 126)) . words $ input
+
+
