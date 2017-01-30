3bdf94b1144ffa0c0d04c1c663d2fe8ffbd9ab5b
mike-k-houghton <mike_k_houghton@yahoo.co.uk>
2017-01-30 08:09:50 +0000

    started on random selections

--- markov.hs
+++ markov.hs
@@ -2,34 +2,54 @@
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
 
 import qualified Data.Map as M
-import Data.List ( sortBy)
+import Data.List (sortBy)
 import Data.Char
 import Data.Ord  (Down (..), comparing)
--- ls = [("a", 1),("b",2 ),("c",3 ),("d",4 ),("e",5 ),("f", 6)]
-data MChain a = Map a [(a, Int)] deriving (Show)
-type Words = [String]
+import System.Random
+import Control.Monad.Random 
 
-links :: Words -> [(String, [(String, Int)] )]
+type Randm a = Rand StdGen a
+
+type Link a   = (a, Int)
+data MChain a = Map a [Link a] deriving (Show)
+type Words    = [String]
+
+ 
+links :: Words -> [(String, [Link String] )]
 links []        = []
-links (x:y:[])  = [(x, [(y, 1)])]
+links [x, y]    = [(x, [(y, 1)])]
 links (w:w':ws) = (w, [(w', 1)]):links (w':ws)
 links _         = []
 
-merge :: [(String, Int)] -> [(String, Int)] -> [(String, Int)]
+merge :: [Link String] -> [Link String] -> [Link String]
 merge ps ps' = condense $ ps ++ ps'
 
 -- -- -- i.e. [("x", 1), ("y", 1) ("x",3)] -> [("x", 4), ("y", 1)] 
-condense :: [(String, Int)] -> [(String, Int)]
+condense :: [Link String] -> [Link String]
 condense = sortBy (comparing (Down . snd))  . M.toList . M.fromListWith (+) 
 
 
-chainWords :: Words -> M.Map String [(String, Int)]
+chainWords :: Words -> M.Map String [Link String]
 chainWords = M.fromListWith merge . links 
+--("so",[("much.",4),("nice.",3), ("sad",3), ("little", 2), ("dear", 1)])
+select :: Int -> [Link String] -> String
+select i  ((w, c) :ls)= selected i c ((w, c) :ls) where
+    selected i ix ((w, c) :ls)
+        | i <= ix = w
+        | otherwise = selected i (ix + c) ls
+
+randomN :: Int -> Randm Int
+randomN n  = getRandomR (1, n)
+
+upperBound :: [Link a] -> Int
+upperBound =foldr (\x a -> a + snd x) 0 
+-- upperBound ls =foldr (\x a -> a + snd x) 0 ls
 
 main :: IO ()
 main = do
-  input <- readFile "speeches.txt"
-
+  --input <- readFile "speeches.txt"
+  let input = "Thank you so much.  That's so nice.  Isn't he a great guy.  He doesn't get a fair press;"
   print $ chainWords . map (filter (\x -> ord x > 32 && ord x < 126)) . words $ input
-
+ 
+  
 
