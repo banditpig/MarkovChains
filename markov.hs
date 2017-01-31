aac23597d5a92011e11bc466c08aca20fd1d2a9f
mike-k-houghton <mike_k_houghton@yahoo.co.uk>
2017-01-31 08:03:41 +0000

    tidy

--- markov.hs
+++ markov.hs
@@ -2,7 +2,7 @@
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
 
 import qualified Data.Map as M
-import Data.List (sortBy)
+import Data.List (sortBy, isInfixOf)
 import Data.Char
 import Data.Ord  (Down (..), comparing)
 import System.Random
@@ -12,7 +12,7 @@ import Control.Monad.Random
 type Randm a = Rand StdGen a
 
 type Link a   = (a, Int)
-data MChain a = Map a [Link a] deriving (Show)
+--data MChain a = MarkovChain a( M.Map a [Link a] ) deriving (Show)
 type Words    = [String]
 
 links :: Words -> [(String, [Link String] )]
@@ -48,37 +48,36 @@ randomWord lnks = do
     x <- randomRIO (1, upperBound lnks)
     return $ select x lnks
 
-randomSentence :: String -> M.Map String [Link String] -> IO String
-randomSentence seed mchain = randomSentence' seed 0 seed mchain where
+randomSentences :: String -> M.Map String [Link String] -> IO String
+randomSentences seed mchain = randomSentence' seed 0 seed mchain where
       randomSentence' :: String -> Int -> String -> M.Map String [Link String] -> IO String
       randomSentence' rslt len sd chn 
-        | len == 10 = do return rslt
+        | len >= 50 && hasFullStop sd = return rslt
         | otherwise = do 
-            let lnks =  (M.!)  mchain sd
+            let lnks = (M.!)  mchain sd
             newWd <- randomWord lnks
-            randomSentence' (f rslt newWd) (len+1) newWd chn where
-               f s1 s2 = s1 ++ " " ++ s2
-
-
-
- -- sample2 freqs = do
- --     idx <- randomRIO (0, sum (map snd freqs) - 1)
- --    return $ indexFreqs idx freqs
+            randomSentence' (rslt ++ " " ++ newWd) (len+1) newWd chn 
+        where hasFullStop = isInfixOf "."  
 
 
 main :: IO ()
 main = do
   input <- readFile "speeches.txt"
   -- let input = "Thank you so much.  That's so nice.  Isn't he a great guy.  He doesn't get a fair press;"
-  let mchain = chainWords . map (filter (\x -> ord x > 32 && ord x < 126)) . words $ input
-  sen1 <- randomSentence "Thank" mchain
-  sen2 <- randomSentence "Thank" mchain
-  sen3 <- randomSentence "Thank" mchain
+  let mchain = chainWords . map (filter (\x -> ord x > 32 && ord x < 126 && ord x /= 34)) . words $ input
+  -- 
+  let startWds = M.keys mchain 
+  sx <- randomRIO (1, length startWds)
 
-
+  sen1 <- randomSentences ( (!!)  startWds sx ) mchain
   print sen1
-  print sen2
-  print sen3
+  -- sen2 <- randomSentence "president" mchain
+  -- sen3 <- randomSentence "love" mchain
+
+
+  -- print sen1
+  -- print sen2
+  -- print sen3
 
  
   
